74b462220e5662b9136d4ffa1080e773f3444df6
Benjamin Southworth <inthecompanyof@me.com>
2017-02-07 16:47:53 +0100

    testing
    testing

--- web.rb
+++ web.rb
@@ -3,7 +3,7 @@ require 'sinatra/base'
 module SlackGoogleBot
   class Web < Sinatra::Base
     get '/' do
-      'Slack integration with Google, https://github.com/dblock/slack-google-bot.'
+      'Slack integration with Google, https://github.com/inthecompanyof/slack-google-bot.'
     end
   end
 end
